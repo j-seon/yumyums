7b55c3579bca323ebe45fb8c097b6b97b05cf276
j-seon <jseon382@gmail.com>
2024-07-02 16:08:42 +0900

    build: MySQL, JPA, and Lombok Dependencies

---
+++ src/main/java/com/yum/yumyums/YumyumsApplication.java
@@ -0,0 +1,13 @@
+package com.yum.yumyums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YumyumsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YumyumsApplication.class, args);
+	}
+
+}
