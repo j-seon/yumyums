becc89981b9b2e09cd690abb59bee88a41ec78fd
kiju <kiju2912@naver.com>
2024-07-09 18:20:41 +0900

    feat: add config file

---
+++ src/main/java/com/yum/yumyums/YumyumsApplication.java
@@ -0,0 +1,14 @@
+package com.yum.yumyums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class YumyumsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YumyumsApplication.class, args);
+	}
+
+}
