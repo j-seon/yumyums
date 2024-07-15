ab8c5b1dcf02aec76866143aee2ed6b019c0c725
j-seon <jseon382@gmail.com>
2024-07-15 14:32:58 +0900

    feat: Add find menus test

--- src/test/java/com/yum/yumyums/service/user/SearchServiceImplTest.java
+++ src/test/java/com/yum/yumyums/service/user/SearchServiceImplTest.java
@@ -25,16 +25,47 @@ class SearchServiceImplTest {
 	private SearchServiceImpl storeService;
 
 	@Test
-	void searchAll() {
-		//TODO 제작 필요
+	@DisplayName("검색어가 들어간 메뉴가 모두 리스트로 저장돼야합니다")
+	void findMenus() {
+		String searchValue = "라면";
+
+		Store store1 = new Store();
+		store1.setId(1);
+		store1.setName("시아");
+		store1.setCategory(FoodCategory.ETC);
+
+		Menu menu1 = new Menu();
+		menu1.setId(1);
+		menu1.setName("떡라면");
+		menu1.setStore(store1);
+
+		Menu menu2 = new Menu();
+		menu2.setId(2);
+		menu2.setName("진심 가득 라면");
+		menu1.setStore(store1);
+
+		Menu menu3 = new Menu();
+		menu3.setId(3);
+		menu3.setName("삼계탕");
+		menu1.setStore(store1);
+
+		when(searchRepository.findByNameLike(searchValue)).thenReturn(Arrays.asList(menu1, menu2));
+
+		// when
+		List<Menu> result = storeService.findMenus(searchValue);
+
+		// then
+		assertThat(result).hasSize(2);
+		assertThat(result).extracting(Menu::getName).containsExactlyInAnyOrder("떡라면", "진심 가득 라면");
+
 	}
 	@Test
 	@DisplayName("검색어가 메뉴명에 포함될시 메뉴를 가진 Store 객체를 반환해야 합니다.")
-	public void searchStoreMenu() throws Exception {
+	public void findStoreMenu() throws Exception {
 		String searchValue = "치킨";
 
 		Store store1 = new Store();
-		store1.setId(2);
+		store1.setId(1);
 		store1.setName("시아");
 		store1.setCategory(FoodCategory.ETC);
 
@@ -56,7 +87,7 @@ class SearchServiceImplTest {
 
 	@Test
 	@DisplayName("검색어가 상점명에 포함될시 해당 상점명의 Store 객체를 반환해야 합니다.")
-	public void searchStoreName() throws Exception {
+	public void findStoreName() throws Exception {
 		String searchValue = "최고";
 
 		Store store1 = new Store();
@@ -82,7 +113,7 @@ class SearchServiceImplTest {
 
 	@Test
 	@DisplayName("한 상점이 중복적으로 나오지 않아야합니다")
-	public void duplicateExamination() throws Exception {
+	public void storeDuplicateExamination() throws Exception {
 		String searchValue = "최고";
 
 		Store store1 = new Store();
